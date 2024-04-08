f55459d8c522113cd862d5a14daa909ae551f6a7
itchytoo <148830549+itchytoo@users.noreply.github.com>
2024-04-07 21:19:32 -0700

    added createTopic2

--- functions/main.py
+++ functions/main.py
@@ -54,3 +54,20 @@ def addmessage(req: https_fn.Request) -> https_fn.Response:
 
     # Send back a message that we've successfully written the message
     return https_fn.Response(f"Message with ID {doc_ref.id} added.")
+
+@https_fn.on_request()
+def createTopic2(req: https_fn.Request) -> https_fn.Response:
+    """Take the text parameter passed to this HTTP endpoint and insert it into
+    a new document in the messages collection."""
+    # Grab the text parameter.
+    original = req.args.get("text")
+    if original is None:
+        return https_fn.Response("No text parameter provided", status=400)
+
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+
+    # Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    _, doc_ref = firestore_client.collection("deliberations").add({"original": original})
+
+    # Send back a message that we've successfully written the message
+    return https_fn.Response(f"topic with ID {doc_ref.id} added.")
