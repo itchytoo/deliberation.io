9a0642a32135c15fbda38a921f5ae4e3a419bdee
itchytoo <148830549+itchytoo@users.noreply.github.com>
2024-04-07 20:34:29 -0700

    real real setup

--- functions/main.py
+++ functions/main.py
@@ -16,63 +16,25 @@ def on_request_example(req: https_fn.Request) -> https_fn.Response:
 
 
 @https_fn.on_request()
-def addmessage(req: https_fn.Request) -> https_fn.Response:
-    """Take the text parameter passed to this HTTP endpoint and insert it into
-    a new document in the messages collection."""
-    # Grab the text parameter.
-    original = req.args.get("text")
-    if original is None:
-        return https_fn.Response("No text parameter provided", status=400)
-
-    firestore_client: google.cloud.firestore.Client = firestore.client()
-
-    # Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    _, doc_ref = firestore_client.collection("messages").add({"original": original})
-
-    # Send back a message that we've successfully written the message
-    return https_fn.Response(f"Message with ID {doc_ref.id} added.")
-
-
-@firestore_fn.on_document_created(document="messages/{pushId}")
-def makeuppercase(
-    event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None],
-) -> None:
-    """Listens for new documents to be added to /messages. If the document has
-    an "original" field, creates an "uppercase" field containg the contents of
-    "original" in upper case."""
-
-    # Get the value of "original" if it exists.
-    if event.data is None:
-        return
-    try:
-        original = event.data.get("original")
-    except KeyError:
-        # No "original" field, so do nothing.
-        return
-
-    # Set the "uppercase" field.
-    print(f"Uppercasing {event.params['pushId']}: {original}")
-    upper = original.upper()
-    event.data.reference.update({"uppercase": upper})
-
-
-@https_fn.on_call()
 def createTopic(req: https_fn.Request) -> https_fn.Response:
     """Take the JSON payload passed to this HTTP endpoint and insert it into
     a new document in the messages collection."""
     try:
-        # Parse JSON body
-        topic = req.args.get("text")
+        data = req
     except ValueError:
         return https_fn.Response("Invalid JSON", status=400)
-
-    if not topic:
+    if not data:
         return https_fn.Response("No topic information provided", status=400)
     print("topic not empty")
     firestore_client: google.cloud.firestore.Client = firestore.client()
-
+    print("working")
+    
     # Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    _, doc_ref = firestore_client.collection("deliberations").add({"topic": topic})
-
+    _, doc_ref = firestore_client.collection("deliberations").add(
+        {"topic": data["text"]}
+    )
     # Send back a message that we've successfully written the message
-    return https_fn.Response(f"Topic with ID {doc_ref.id} added.")
+    # return https_fn.Response(f"Message with ID {doc_ref.id} added.")
+    # return make_response(
+    #     jsonify({"message": f"Topic with ID {doc_ref.id} added."}), 200
+    # )
