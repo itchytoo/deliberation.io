af6a996137340ecbf4bb269b03e7ff5bd14eff32
Chinmaya Andukuri <andukuri.stanford.edu>
2024-04-14 21:18:44 -0700

    saveComment functionality

--- functions/fn_impl/round1.py
+++ functions/fn_impl/round1.py
@@ -92,3 +92,68 @@ def getRound1Information(req: https_fn.Request) -> https_fn.Response:
 
     except ValueError:
         return https_fn.Response("No JWT token provided", status=401)
+
+
+@https_fn.on_request(cors=enableCors)
+def saveComment(req: https_fn.Request) -> https_fn.Response:
+    """Take the JSON object passed to this HTTP endpoint and insert it into
+    a new document in the messages collection. Expects a POST request."""
+    try:
+        # authenticate the user
+        token = req.headers.get("Authorization").split("Bearer ")[1]
+        decoded_token = auth.verify_id_token(token)
+        user_id = decoded_token["user_id"]
+
+        # Parse JSON directly from request body
+        data = req.get_json()
+        required_keys = set(
+            [
+                "deliberationDocRef"
+                "rawText",
+            ]
+        )
+        # Ensure the JSON object contains a 'topic' field
+        if set(list(data.keys())) != required_keys:
+            return https_fn.Response("Required keys missing in JSON object", status=400)
+
+        # add the adminID field to the data
+        data["adminID"] = user_id
+
+        # Initialize Firestore client
+        firestore_client = firestore.client()
+
+        # add the new deliberation to the collection
+        user_comment_doc = firestore_client.collection("deliberations").document(data["deliberationDocRef"]).collection("commentCollection").document(data["adminID"]).get().to_dict()
+        if "comments" not in user_comment_doc.keys():
+            user_comment_doc["comments"] = list()
+        user_comment_doc.append(data["rawText"])
+    
+        # update the createdDeliberations field
+        firestore_client.collection("deliberations").document(data["deliberationDocRef"]).collection("commentCollection").document(data["adminID"]).update(
+            user_comment_doc
+        )
+
+
+        # Send back a message that we've successfully added the comment.
+        return https_fn.Response(f"Comment successfully added.")
+
+    # Catch any errors that occur during the process
+    except auth.InvalidIdTokenError:
+        return https_fn.Response("Invalid JWT token", status=401)
+
+    except auth.ExpiredIdTokenError:
+        return https_fn.Response("Expired JWT token", status=401)
+
+    except auth.RevokedIdTokenError:
+        return https_fn.Response("Revoked JWT token", status=401)
+
+    except auth.CertificateFetchError:
+        return https_fn.Response(
+            "Error fetching the public key certificates", status=401
+        )
+
+    except auth.UserDisabledError:
+        return https_fn.Response("User is disabled", status=401)
+
+    except ValueError:
+        return https_fn.Response("No JWT token provided", status=401)
