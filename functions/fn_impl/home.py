7217abd1522508c96783e10c92f2ff7e2995aca1
itchytoo <148830549+itchytoo@users.noreply.github.com>
2024-05-02 17:50:29 -0700

    fixed getParticipated and getCreated

--- functions/fn_impl/home.py
+++ functions/fn_impl/home.py
@@ -107,7 +107,7 @@ def getCreatedTopics(req: https_fn.Request) -> https_fn.Response:
                 .to_dict()
             )
             topic_list.append(
-                {"deliberationID": topic_id, "topicName": topic_doc["topic"]}
+                {"deliberationID": topic_id, "topicName": topic_doc["topicName"]}
             )
 
         # send back a JSON object with the doc references and also the topic names
@@ -169,7 +169,7 @@ def getParticipatedTopics(req: https_fn.Request) -> https_fn.Response:
                 .to_dict()
             )
             topic_list.append(
-                {"deliberationID": topic_id, "topicName": topic_doc["topic"]}
+                {"deliberationID": topic_id, "topicName": topic_doc["topicName"]}
             )
 
         # send back a JSON object with the doc references and also the topic names
