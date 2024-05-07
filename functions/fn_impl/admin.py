409739bb72337a7e8d97dbef5cdb59b3ab0f17c0
itchytoo <148830549+itchytoo@users.noreply.github.com>
2024-05-06 21:47:25 -0700

    added new function to get description

--- functions/fn_impl/admin.py
+++ functions/fn_impl/admin.py
@@ -9,71 +9,31 @@ enableCors = options.CorsOptions(
     )
 
 @https_fn.on_request(cors=enableCors)
-def getStageInfo(req: https_fn.Request) -> https_fn.Response:
-    """Take the JSON object passed to this HTTP endpoint and insert it into
-    a new document in the messages collection. Expects a POST request."""
+def getDescription(req: https_fn.Request) -> https_fn.Response:
     try:
         # authenticate the user
         token = req.headers.get("Authorization").split("Bearer ")[1]
         decoded_token = auth.verify_id_token(token)
         user_id = decoded_token["user_id"]
 
-        # Parse JSON directly from request body
-        data = req.get_json()
-        required_keys = set(
-            [
-                "deliberationDocRef"
-            ]
-        )
-        
-        
-        # Ensure the JSON object contains a 'topic' field
-        if set(list(data.keys())) != required_keys:
-            return https_fn.Response("Required keys missing in JSON object", status=400)
-        
-
         # Initialize Firestore client
         firestore_client = firestore.client()
 
+        # parse JSON directly from request body
+        data = req.get_json()
+        deliberationDocRef = data["deliberationDocRef"]
 
-        # add the doc reference to the topic_drefs collection
-        deliberationInfo = firestore_client.collection("deliberations").document(data["deliberationDocRef"]).get().to_dict()
-        desiredStages = [stage for stage, choice in zip(STAGES, deliberationInfo["delibFlow"]) if choice != "None"]
-        result = [
-            {
-                "stageName" : "test", 
-                "isCurrent" : True if deliberationInfo["currStage"] == key else False
-            }
-            for key in deliberationInfo
-        ]
+        # retrieve the topic information for the desired deliberation
+        topic_doc = firestore_client.collection("deliberations").document(deliberationDocRef).get().to_dict()
+        description = topic_doc["description"]
+
+        # send back a JSON object with the doc references and also the topic names
+        return https_fn.Response(description)
         
-        # Send back a message that we've successfully written the document
-        return https_fn.Response(
-            json.dumps(result), content_type="application/json"
-        )
-
-    # Catch any errors that occur during the process
-    except auth.InvalidIdTokenError:
-        return https_fn.Response("Invalid JWT token", status=401)
-
-    except auth.ExpiredIdTokenError:
-        return https_fn.Response("Expired JWT token", status=401)
-
-    except auth.RevokedIdTokenError:
-        return https_fn.Response("Revoked JWT token", status=401)
-
-    except auth.CertificateFetchError:
-        return https_fn.Response(
-            "Error fetching the public key certificates", status=401
-        )
-
-    except auth.UserDisabledError:
-        return https_fn.Response("User is disabled", status=401)
-
-    except ValueError:
-        return https_fn.Response("No JWT token provided", status=401)
+    except Exception as e:
+        return https_fn.Response(str(e), status=400) 
     
-    
+
 @https_fn.on_request(cors=enableCors)
 def getDelibInfo(req: https_fn.Request) -> https_fn.Response:
     try:
